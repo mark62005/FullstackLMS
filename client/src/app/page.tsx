2bdfb6ad879ab1787bb2fcfd7acf08bc0bc76046
Mark Wong <markwong.digital@gmail.com>
2024-11-26 23:27:22 -0800

    Display landing page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,14 @@
 import NonDashboardNavbar from "@/components/shared/navbar/NonDashboardNavbar";
+import Landing from "./(non-dashboard)/landing/page";
 
 export default function Home() {
 	return (
 		<div className="flex flex-col min-h-screen w-full">
 			<NonDashboardNavbar />
+
+			<main className="flex flex-grow w-full h-full justify-center items-center">
+				<Landing />
+			</main>
 		</div>
 	);
 }
