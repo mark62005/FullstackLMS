b7cab375b428e48374c8b7ca35f4e9ff441181ad
Mark Wong <markwong.digital@gmail.com>
2024-11-28 13:36:01 -0800

    Create a custom base query for api

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,8 +1,34 @@
-import { Course } from "@/types";
+import { BaseQueryApi, FetchArgs } from "@reduxjs/toolkit/query";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { type Course } from "@/types";
+
+const customBaseQuery = async (
+	args: string | FetchArgs,
+	api: BaseQueryApi,
+	extraOptions: any
+) => {
+	const baseQuery = fetchBaseQuery({
+		baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL,
+	});
+
+	try {
+		const result: any = await baseQuery(args, api, extraOptions);
+
+		if (result.data) {
+			result.data = result.data.data;
+		}
+
+		return result;
+	} catch (error: unknown) {
+		const errorMessage =
+			error instanceof Error ? error.message : "Unknown Error occured";
+
+		return { error: { status: "FETCH_ERROR", message: errorMessage } };
+	}
+};
 
 export const api = createApi({
-	baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL }),
+	baseQuery: customBaseQuery,
 	reducerPath: "api",
 	tagTypes: ["Courses"],
 	endpoints: (build) => ({
