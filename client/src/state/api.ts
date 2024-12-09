e8545bef3e4e5045118cf1722e50801b8170f03a
Mark Wong <markwong.digital@gmail.com>
2024-12-09 02:15:55 -0800

    Display message when getting results from api

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -2,6 +2,7 @@ import { BaseQueryApi, FetchArgs } from "@reduxjs/toolkit/query";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { type User } from "@clerk/nextjs/server";
 import { Clerk } from "@clerk/clerk-js";
+import { toast } from "sonner";
 
 const customBaseQuery = async (
 	args: string | FetchArgs,
@@ -23,8 +24,31 @@ const customBaseQuery = async (
 	try {
 		const result: any = await baseQuery(args, api, extraOptions);
 
+		if (result.error) {
+			const errorData = result.error.data;
+			const errorMessage =
+				errorData?.message ||
+				result.error.status.toString() ||
+				"An error occurred";
+
+			toast.error(`Error: ${errorMessage}`);
+		}
+
+		const isMutationRequest =
+			(args as FetchArgs).method && (args as FetchArgs).method !== "GET";
+
+		if (isMutationRequest) {
+			const successMessage = result.data?.message;
+			if (successMessage) toast.success(successMessage);
+		}
+
 		if (result.data) {
 			result.data = result.data.data;
+		} else if (
+			result.error?.status === 204 ||
+			result.meta?.response?.status === 24
+		) {
+			return { data: null };
 		}
 
 		return result;
