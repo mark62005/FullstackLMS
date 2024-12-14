e206231221f50c8014569900a13e8af4f311c8b6
Mark Wong <markwong.digital@gmail.com>
2024-12-13 17:25:24 -0800

    Build endpoints for create, update and delete (In progress) courses

--- server/src/routes/courseRoutes.ts
+++ server/src/routes/courseRoutes.ts
@@ -1,9 +1,22 @@
 import express from "express";
-import { getCourse, listCourses } from "../controllers/courseController";
+import { requireAuth } from "@clerk/express";
+import multer, { memoryStorage } from "multer";
+import {
+	createCourse,
+	deleteCourse,
+	getCourse,
+	listCourses,
+	updateCourse,
+} from "../controllers/courseController";
 
 const router = express.Router();
+const upload = multer({ storage: memoryStorage() });
 
 router.get("/", listCourses);
+router.post("/", requireAuth(), createCourse);
+
 router.get("/:courseId", getCourse);
+router.put("/:courseId", requireAuth(), upload.single("image"), updateCourse);
+router.delete("/:courseId", requireAuth(), deleteCourse);
 
 export default router;
